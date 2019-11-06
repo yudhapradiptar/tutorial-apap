2f0f6a40e3926bdd7b759bc7f4ab4a3ca5f1f008
Yudha Pradipta Ramadan <yudha512000@gmail.com>
2019-11-06 09:32:35 +0700

    benerin error

--- gopud/src/main/java/apap/tutorial/gopud/service/MenuRestService.java
+++ gopud/src/main/java/apap/tutorial/gopud/service/MenuRestService.java
@@ -3,13 +3,14 @@ package apap.tutorial.gopud.service;
 import apap.tutorial.gopud.model.MenuModel;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MenuRestService {
     MenuModel createMenu(MenuModel menu);
 
     List<MenuModel> retriveListMenu();
 
-    MenuModel getMenuByIdMenu(Long idMenu);
+    Optional<MenuModel> getMenuByIdMenu(Long idMenu);
 
     MenuModel changeMenu(Long idMenu, MenuModel menuUpdate);
 
